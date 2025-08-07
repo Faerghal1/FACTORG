4995a4871c1d6f67bb3298423189160fb8fbbb93
Caleb-Simpson <csimpson@students.mags.school.nz>
2025-08-08 09:39:36 +1200

    Finished index

--- Scripts/Index.gd
+++ Scripts/Index.gd
@@ -1,28 +1,42 @@
 extends Control
 
+var Recipe = 0
 
-func _on_iron_ore_pressed():
-	if $IronOre.visible == false:
+
+func _process(_delta):
+	if Recipe == 1:
 		$IronOre.show()
-	else:
-		$IronOre.hide()
+	if Recipe == 2:
+		$CopperOre.show()
+	if Recipe == 3:
+		$IronIngot.show()
+	if Recipe == 4:
+		$CopperIngot.show()
+	if Recipe == 5:
+		$IronRod.show()
+	if Recipe == 6:
+		$CopperWire.show()
 
 
-func _on_copper_ore_pressed():
-	pass # Replace with function body.
+func _on_iron_ore_mouse_entered():
+	Recipe = 1
 
 
-func _on_iron_ingot_pressed():
-	pass # Replace with function body.
+func _on_copper_ore_mouse_entered():
+	Recipe = 2
 
 
-func _on_copper_ingot_pressed():
-	pass # Replace with function body.
+func _on_iron_ingot_mouse_entered():
+	Recipe = 3
 
 
-func _on_iron_rod_pressed():
-	pass # Replace with function body.
+func _on_copper_ingot_mouse_entered():
+	Recipe = 4
 
 
-func _on_copper_wire_pressed():
-	pass # Replace with function body.
+func _on_iron_rod_mouse_entered():
+	Recipe = 5
+
+
+func _on_copper_wire_mouse_entered():
+	Recipe = 6
